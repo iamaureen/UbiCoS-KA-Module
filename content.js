2ee2b74ab5556204f9ad523a3d75f0a7ba1952da
iamaureen <ishrat.ahmed@asu.edu>
2020-10-29 18:54:45 -0400

    send data from popup.js to content.js

--- content.js
+++ content.js
@@ -26,125 +26,131 @@ chrome.runtime.onMessage.addListener(
       // Directly respond to the sender (popup) through the specified callback.
       sendResponse(domInfo);
     }
+
+
+    if (request.username !== undefined) {
+            console.log(request.username);
+
+        }
+
   }
 );
 
 
+window.addEventListener ("load", myMain, false);
 
+function myMain (evt) {
+  //detects textarea out of focus event
+  setTimeout(function(){
+    var username = $("._wozql4").text();
+    console.log("timer function set,", username);
+    //alert('Hello, we are excited to be here with you, ', username);
 
-//detects textarea out of focus event
-setTimeout(function(){
-  console.log("timer function set");
+    var pageTitle = document.title;
+    console.log(pageTitle);
+    var pageURL = location.href;
 
-  var pageTitle = document.title;
-  var pageURL = location.href;
+    var comment_target_id = '';
+    var comment = '';
 
-  var comment_target_id = '';
-  var comment = '';
+    $(document).on("blur", "textarea", function(e){
+      //console.log (e);
+      console.log('user selected textarea :: ', e.target.id);
+      comment_target_id = e.target.id;
+      comment = $('textarea#'+e.target.id).val();
 
-  var username = $("._wozql4").text();
+      //we will attach our element with this node; so adjust depending on
+      //where we want to place the badge
+      parentNode = e.target.parentNode.parentNode;
+      if(!comment.trim()) {
+        //comment is empty; do nothing
+      }else{
+        //comment is not empty
+        //make the database call and save the students' response;
+        console.log('User input is not empty, Currently logged in :: ', username);
+        //make an ajax call to get students info loading (if needed);
 
-  $(document).on("blur", "textarea", function(e){
-    //console.log (e);
-    console.log(e.target.id);
-    comment_target_id = e.target.id;
-    comment = $('textarea#'+e.target.id).val();
-
-    //we will attach our element with this node; so adjust depending on
-    //where we want to place the badge
-    parentNode = e.target.parentNode.parentNode;
-    if(!comment.trim()) {
-      //comment is empty; do nothing
-    }else{
-      //comment is not empty
-      //make the database call and save the students' response;
-      console.log(username);
-      $.ajax({
-          //url: 'https://hcilabasu.pythonanywhere.com/saveKApost',
-          url: 'http://127.0.0.1:8000/saveKApost',
-          type: 'POST',
-          async: false,
-          data: {'username': username, //TODO: 1) split, take the first part, lowercase
-          'pageURL' : pageURL,
-          'textareaId' : comment_target_id,
-          'content' : comment },
-          success: function (data) {
-              console.log(data);
-          }
-
-        });//end of the first ajax call
-
-        //this ajax call will check for keyword and assign badges as necessary
+        //the following ajax call saves user input to the database
         $.ajax({
-            //url: 'https://hcilabasu.pythonanywhere.com/saveKApost',
-            url: 'http://127.0.0.1:8000/matchKeywords',
+            url: 'https://hcilabasu.pythonanywhere.com/saveKApost',
+            //url: 'http://127.0.0.1:8000/saveKApost',
             type: 'POST',
             async: false,
-            data: {'username': username, 'message': comment, 'platform': 'KA', 'ka_url': pageURL},
+            data: {'username': username, //TODO: 1) split, take the first part, lowercase
+            'pageURL' : pageURL,
+            'textareaId' : comment_target_id,
+            'content' : comment },
             success: function (data) {
-
-              //get all the data
-              console.log(data);
-              console.log(data.isMatch);
-              console.log(data.praiseText);
-              console.log(data.selected_badge);
-              //attach the element with the node above (parentNode)
-              //remove any previously added div tags here
-              document.querySelectorAll('.dynamic-divTag').forEach(function(elem){
-                elem.remove();
-              });
-              //attach the new element
-              //1. create the new element div
-              var dynamic_div = document.createElement('div');
-              dynamic_div.className = "dynamic-divTag"; //add the class name for div
-              //2. create an image tag (that will hold the badge)
-              var image = document.createElement("img");
-              //todo: set the image name dynamically based on the badge
-              image.src = chrome.runtime.getURL("images/"+data.selected_badge+".png");
-              image.className = "dynamic-img"; //add the class name for image
-              dynamic_div.appendChild(image);
-              //3. create a p tag for the praise
-              var ptag = document.createElement("p");
-              ptag.innerHTML = data.praiseText; //set this from the server
-              ptag.className = "dynamic-pTag";
-              dynamic_div.appendChild(ptag);
-              //3a. create a p tag with the badge name
-              var pbadge = document.createElement("p");
-              pbadge.innerHTML = 'You earned the ' +data.selected_badge+ ' badge!'; //set this from the server
-              pbadge.className = "dynamic-pbadge";
-              dynamic_div.appendChild(pbadge);
-              //3b. create a p tag for instruction
-              var pins = document.createElement("p");
-              pins.innerHTML = 'You can go to Modelbook and try another badge!'
-              pins.className = "dynamic-pins";
-              dynamic_div.appendChild(pins);
-              //4. add the div into the desired element of the DOM
-              parentNode.appendChild(dynamic_div);
-
+                console.log(data +', '+ username);
             }
 
-          });//end of the second jax call
+          });//end of the first ajax call
 
-            // success: function(response){
-            //        console.log(response.isMatch); //returns true if match found, else false
-            //        if(response.isMatch){
-            //            console.log('inside the if else loop');
-            //            $("#gallery-reward").css("display", "block");
-            //            //set up the values
-            //            $('#reward-div-selection').text('You earned the '+global_badge_selected+' badge!');
-            //            $('#reward-div-prompt').text(response.praiseText);
-            //
-            //        }
-            //     }
+          //this ajax call will check for keyword and assign badges as necessary
+          $.ajax({
+              url: 'https://hcilabasu.pythonanywhere.com/matchKeywords',
+              //url: 'http://127.0.0.1:8000/matchKeywords',
+              type: 'POST',
+              async: false,
+              data: {'username': username, 'message': comment, 'platform': 'KA', 'ka_url': pageURL},
+              success: function (data) {
 
-    }//end of if comment is empty or not
+                //get all the data from the server
+                console.log(data, username);
+                console.log(data.isMatch);
+                console.log(data.praiseText);
+                console.log(data.selected_badge);
+
+                if(data.isMatch){ //if the user message matches keyword
+
+                    //attach the element with the node above (parentNode)
+                    //remove any previously added div tags here
+                    document.querySelectorAll('.dynamic-divTag').forEach(function(elem){
+                      elem.remove();
+                    });
+                    //attach the new element
+                    //1. create the new element div
+                    var dynamic_div = document.createElement('div');
+                    dynamic_div.className = "dynamic-divTag"; //add the class name for div
+                    //2. create an image tag (that will hold the badge)
+                    var image = document.createElement("img");
+                    //todo: set the image name dynamically based on the badge
+                    image.src = chrome.runtime.getURL("images/"+data.selected_badge+".png");
+                    image.className = "dynamic-img"; //add the class name for image
+                    dynamic_div.appendChild(image);
+                    //3. create a p tag for the praise
+                    var ptag = document.createElement("p");
+                    ptag.innerHTML = data.praiseText; //set this from the server
+                    ptag.className = "dynamic-pTag";
+                    dynamic_div.appendChild(ptag);
+                    //3a. create a p tag with the badge name
+                    var pbadge = document.createElement("p");
+                    pbadge.innerHTML = 'You earned the ' +data.selected_badge+ ' badge!'; //set this from the server
+                    pbadge.className = "dynamic-pbadge";
+                    dynamic_div.appendChild(pbadge);
+                    //3b. create a p tag for instruction
+                    var pins = document.createElement("p");
+                    pins.innerHTML = 'You can go to Modelbook and try another badge!'
+                    pins.className = "dynamic-pins";
+                    dynamic_div.appendChild(pins);
+                    //4. add the div into the desired element of the DOM
+                    parentNode.appendChild(dynamic_div);
+
+                } // end of the if statement
+
+              }
+
+            });//end of the second jax call
+
+      }//end of if comment is empty or not
 
 
 
-  });//end of the text blur on focus call
+    });//end of the text blur on focus call
 
-  //the comment button capture did not work
-  //inactive comment button class _1a9oxwpk
-  //active comment button class _h6mih68
+    //the comment button capture did not work
+    //inactive comment button class _1a9oxwpk
+    //active comment button class _h6mih68
 
-}, 5000);//end of the timer method
+  }, 10000);//end of the timer method, after 10 sec
+}
--- popup.js
+++ popup.js
@@ -1,7 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
 // Once the DOM is ready...
@@ -16,51 +12,23 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // let templateBtn = document.getElementById('templateBtn');
-  let badgeBtn = document.getElementById('badgeBtn');
 
-  // templateBtn.onclick = function(element) {
-  //   alert( "Handler for .click() called, templateBtn" );
-  //   //step 1: send to background.js
-  //   chrome.runtime.sendMessage({clicked : true});
-  // };
+  document.getElementById("submitBtn").addEventListener("click", function() {
+    //get the user name from the input field
+    var user_name_popup = document.getElementById('user_name').value;
+    console.log('open in the inspect', user_name_popup);
 
-  badgeBtn.onclick = function(element) {
-      //alert( "Handler for .click() called, badgeBtn" );
-      //display the div with badge options
-      $(".div-badge-options").css("display", "");
-      $(".div-badge-prompt").css("display", "none");
+      chrome.tabs.query({
+          active: true,
+          currentWindow: true
+      }, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+              username: user_name_popup
+          });
+      });
+  });
 
-      //Make an ajax call here and get the value the student just posted
-      // $.ajax({
-      //       type: "GET",
-      //       url: "https://www.khanacademy.org/api/internal/discussions/kaencrypted_b4ba0c8e540911b420592ad0e8c83d4f_44abda91a89ee38abaf33dfbd294ad7a0f9f7c9b8b67bb6872288fd3e84b89a01f538174acdf203ca5abde8fd7e8f50f7ee501d236736ab85cc96e31c9dfc4f5f9172fa696703669728bf4357ab892aa44f71efa15818a04e23997dd20f9bb34e72740e4afe5c256e9a7a2316a29dd09a397ed1ff4a28cbe76ea6f425c7d3d34/replies?casing=camel&lang=en&_=200316-1111-697e67c8399e_1584404407215", //TODO: make this link dynamic.
-      //       success: function(msg){
-      //           var data = JSON.stringify(msg);
-      //           alert(data);
-      //         }
-      //    });
 
-         $.ajax({
-               type: "GET",
-               url: "http://127.0.0.1:8000/getUserList", //TODO: make this link dynamic.
-               success: function(msg){
-                   //var data = JSON.stringify(msg);
-                   //alert(data);
-
-                 }
-            });
-  };
-
-  $('.div-badge-options a').off().on('click', function(e){
-
-        console.log($(e.target).parents('a').attr('id'));
-        $(".div-badge-options").css("display", "none");
-        $(".div-badge-prompt").css("display", "");
-
-        //alert("clicked");
-
-    });
 
 
 });
